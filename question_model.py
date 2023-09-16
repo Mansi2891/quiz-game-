4d7c5c1bafcfcbf97f3362d186e6a6856d29ff8d
Mansi2891 <mansiingle2891@gmail.com>
2023-09-16 17:22:30 +0530

    Initial commit

---
+++ question_model.py
@@ -0,0 +1,9 @@
+class Question:
+
+    def __init__(self, q_text,q_answer):
+        self.text = q_text
+        self.answer = q_answer
+# here we have created class we have given two attributes and the function is passed .
+##new_q = Question("sddsd","false")
+##print (new_q.text)
+# here we are fetching the values of question .
